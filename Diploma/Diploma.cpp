63d4a3c7d891ffbdf2b4093f5c3702c06b84a40c
Denis Belobrotski <belobrotskiy@gmail.com>
2019-02-25 23:49:35 +0300

    Initial commit

---
+++ Diploma/Diploma.cpp
@@ -0,0 +1,77 @@
+﻿// Diploma.cpp : Defines the entry point for the application.
+//
+
+#include "Diploma.h"
+
+void fillVariables(Variables *variables);
+
+double calcIntegral0(Variables *variables);
+
+void printVector(std::ostream *os, std::vector<double> *vector);
+
+
+int main()
+{
+	Variables variables;
+	variables.s = std::vector<double>(N + 1);
+	variables.r = std::vector<double>(N + 1);
+	variables.z = std::vector<double>(N + 1);
+	variables.beta = std::vector<double>(N + 1);
+
+	fillVariables(&variables);
+
+	printVector(&std::cout, &(variables.s));
+	std::cout << "************************" << std::endl;
+	printVector(&std::cout, &(variables.r));
+	std::cout << "************************" << std::endl;
+	printVector(&std::cout, &(variables.z));
+	std::cout << "************************" << std::endl;
+	printVector(&std::cout, &(variables.beta));
+	std::cout << "************************" << std::endl;
+
+	std::cout << calcIntegral0(&variables) << std::endl;
+
+	system("pause");
+
+	return 0;
+}
+
+
+void fillVariables(Variables *variables)
+{
+	for (int i = 0; i < N + 1; i++)
+	{
+		variables->s[i] = LOWER_BOUND + i * STEP;
+
+		variables->r[i] = variables->s[i];
+
+		variables->z[N - i] = variables->s[i];
+
+		variables->beta[i] = -M_PI_4;
+	}
+}
+
+
+double calcIntegral0(Variables *variables)
+{
+	double result = 0;
+
+	for (int i = 1; i < N + 1; i++)
+	{
+		double a = (variables->z[i - 1] + variables->z[i]) / 2;
+		double b = (variables->r[i - 1] + variables->r[i]) / 2;
+		double c = (variables->beta[i - 1] + variables->beta[i]) / 2;
+		result += a * b * cos(c);
+	}
+
+	return 2 * M_PI * STEP * result;
+}
+
+
+void printVector(std::ostream *os, std::vector<double> *vector)
+{
+	for (const auto& currentComponent : *vector)
+	{
+		*os << currentComponent << std::endl;
+	}
+}
---
+++ Diploma/Diploma.h
@@ -0,0 +1,24 @@
+﻿// Diploma.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+const int N = 100;
+const double LOWER_BOUND = 0;
+const double UPPER_BOUND = 1;
+const double STEP = (UPPER_BOUND - LOWER_BOUND) / N;
+
+
+struct Variables
+{
+	std::vector<double> s;
+	std::vector<double> r;
+	std::vector<double> z;
+	std::vector<double> beta;
+};
