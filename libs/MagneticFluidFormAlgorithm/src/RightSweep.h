ad4a6db5e910bbf20f5398c7ac876d8316c4618d
Denis Belobrotski <denis.b@playgendary.com>
2019-04-19 15:21:25 +0300

    Add submodules

--- libs/MagneticFluidFormAlgorithm/src/RightSweep.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-
-namespace algorithm
-{
-    void calcRightSweep(
-            std::vector<std::vector<double>> &matrix, std::vector<double> &freeMembers, std::vector<double> &result);
-}
