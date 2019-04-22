98e3c65a4a1d20f10f3ab0d11712157e768c4822
Denis Belobrotski <belobrotskiy@gmail.com>
2019-04-22 22:02:28 +0300

    Log calcuations duration Refactoring

--- src/Diploma.cpp
+++ src/Diploma.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <chrono>
 
 #include <GnuplotWrapper/GnuplotWrapper.h>
 #include <MagneticFluidFormAlgorithm/MagneticFluidFormAlgorithm.h>
@@ -33,6 +34,25 @@ plot::Plot* createDefaultPlot(
         std::vector<plot::Graph>* graphs, std::string title, plot::PlotOutputType outputType,
         std::string outputFilePath);
 
+void calcResults(
+        std::vector<algorithm::Variables>& implicitExperimentVariables,
+        std::vector<algorithm::Variables>& explicitExperimentVariables,
+        std::vector<algorithm::IterationInfo>& implicitIterationsInfo,
+        std::vector<algorithm::IterationInfo>& explicitIterationsInfo);
+
+void makePlots(
+        std::vector<algorithm::Variables>& implicitExperimentVariables,
+        std::vector<algorithm::Variables>& explicitExperimentVariables,
+        std::vector<algorithm::IterationInfo>& implicitIterationsInfo,
+        std::vector<algorithm::IterationInfo>& explicitIterationsInfo,
+        bool needPauseExecution);
+
+void calcResiduals(
+        std::vector<algorithm::Variables>& implicitExperimentVariables,
+        std::vector<algorithm::Variables>& explicitExperimentVariables,
+        std::vector<algorithm::IterationInfo>& implicitIterationsInfo,
+        std::vector<algorithm::IterationInfo>& explicitIterationsInfo);
+
 
 int main()
 {
@@ -44,87 +64,16 @@ int main()
         std::vector<algorithm::IterationInfo> implicitIterationsInfo;
         std::vector<algorithm::IterationInfo> explicitIterationsInfo;
 
-        algorithm::DifferenceMethod* implicitDifferenceMethod = new algorithm::ImplicitDifferenceMethod(
-                &implicitExperimentVariables, &implicitIterationsInfo);
-        algorithm::DifferenceMethod* explicitDifferenceMethod = new algorithm::ExplicitDifferenceMethod(
-                &explicitExperimentVariables, &explicitIterationsInfo);
+        calcResults(
+                implicitExperimentVariables, explicitExperimentVariables, implicitIterationsInfo,
+                explicitIterationsInfo);
 
-        implicitDifferenceMethod->setIsNeedResetTau(false);
-        explicitDifferenceMethod->setIsNeedResetTau(false);
+        calcResiduals(
+                implicitExperimentVariables, explicitExperimentVariables, implicitIterationsInfo,
+                explicitIterationsInfo);
 
-        std::function<void(long long, long long)> showIterationsProgressBarFunction =
-                [](long long currentIteration, long long maxIterations)
-                {
-                    utils::showIterationsProgressBar(currentIteration, maxIterations);
-                };
-        implicitDifferenceMethod->setIterationFinishedCallback(&showIterationsProgressBarFunction);
-        explicitDifferenceMethod->setIterationFinishedCallback(&showIterationsProgressBarFunction);
-
-        algorithm::AlgorithmConfigurator algorithmConfigurator;
-        std::vector<algorithm::TargetParameter>* targetParameters =
-                algorithmConfigurator.readAlgorithmSequenceFromFile(
-                        "../res/algorithm_config_full.json", algorithm::ConfigFileTypeJson);
-
-        implicitDifferenceMethod->setTargetParameters(targetParameters);
-        explicitDifferenceMethod->setTargetParameters(targetParameters);
-
-        std::cout << "*****Algorithms info*****" << std::endl;
-
-        std::cout << "Difference scheme: " << std::endl << std::endl;
-        implicitDifferenceMethod->calcResult();
-
-        std::cout << "Angles: " << std::endl;
-        explicitDifferenceMethod->calcResult();
-
-        plot::Plot* implicitMethodPlot = configMagneticFluidPlot(
-                implicitExperimentVariables, implicitIterationsInfo, "Finite-difference method",
-                plot::PlotOutputTypeWindow, "");
-        plot::Plot* explicitMethodPlot = configMagneticFluidPlot(
-                explicitExperimentVariables, explicitIterationsInfo, "Tangential method", plot::PlotOutputTypeWindow,
-                "");
-        plot::Plot* comparisonPlot = configComparisonPlot(
-                implicitExperimentVariables.back(), implicitIterationsInfo.back(), explicitExperimentVariables.back(),
-                explicitIterationsInfo.back(), "Comparison", plot::PlotOutputTypeWindow, "");
-
-        implicitMethodPlot->makeGraphs();
-        explicitMethodPlot->makeGraphs();
-        comparisonPlot->makeGraphs();
-
-        std::cout << "*****Residuals*****" << std::endl;
-        auto size = std::min(explicitIterationsInfo.size(), implicitIterationsInfo.size());
-        for (auto i = 0; i < size; i++)
-        {
-            auto currentOldIteration = implicitIterationsInfo[i].index;
-            auto currentNewIteration = explicitIterationsInfo[i].index;
-
-            double radiusResidual = algorithm::calcResidual(implicitExperimentVariables[currentOldIteration].r,
-                                                            explicitExperimentVariables[currentNewIteration].r);
-            double heightResidual = algorithm::calcResidual(implicitExperimentVariables[currentOldIteration].z,
-                                                            explicitExperimentVariables[currentNewIteration].z);
-            double commonResidual = std::max(radiusResidual, heightResidual);
-
-            std::cout << "Experiment number:" << std::endl;
-            std::cout << "difference scheme: #" << currentOldIteration << ", angles: #" << currentNewIteration
-                      << std::endl;
-            std::cout << "Residual:" << std::endl;
-            std::cout << "radius: " << radiusResidual << ", height: " << heightResidual << std::endl;
-            std::cout << "common: " << commonResidual << std::endl;
-            std::cout << "************************" << std::endl;
-        }
-
-        utils::pauseExecution();
-
-        delete explicitMethodPlot;
-        delete implicitMethodPlot;
-        delete comparisonPlot;
-    }
-    catch (plot::PipeException& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    catch (plot::VectorSizeException& e)
-    {
-        std::cerr << e.what() << std::endl;
+        makePlots(implicitExperimentVariables, explicitExperimentVariables, implicitIterationsInfo,
+                  explicitIterationsInfo, true);
     }
     catch (std::runtime_error& e)
     {
@@ -265,4 +214,122 @@ plot::Plot* createDefaultPlot(
     config->outputFilePath = outputFilePath;
 
     return new plot::Plot(config, graphs);
+}
+
+
+void calcResults(
+        std::vector<algorithm::Variables>& implicitExperimentVariables,
+        std::vector<algorithm::Variables>& explicitExperimentVariables,
+        std::vector<algorithm::IterationInfo>& implicitIterationsInfo,
+        std::vector<algorithm::IterationInfo>& explicitIterationsInfo)
+{
+    algorithm::DifferenceMethod* implicitDifferenceMethod = new algorithm::ImplicitDifferenceMethod(
+            &implicitExperimentVariables, &implicitIterationsInfo);
+    algorithm::DifferenceMethod* explicitDifferenceMethod = new algorithm::ExplicitDifferenceMethod(
+            &explicitExperimentVariables, &explicitIterationsInfo);
+
+    implicitDifferenceMethod->setIsNeedResetTau(false);
+    explicitDifferenceMethod->setIsNeedResetTau(false);
+
+    std::function<void(long long, long long)> showIterationsProgressBarFunction =
+            [](long long currentIteration, long long maxIterations)
+            {
+                utils::showIterationsProgressBar(currentIteration, maxIterations);
+            };
+    implicitDifferenceMethod->setIterationFinishedCallback(&showIterationsProgressBarFunction);
+    explicitDifferenceMethod->setIterationFinishedCallback(&showIterationsProgressBarFunction);
+
+    algorithm::AlgorithmConfigurator algorithmConfigurator;
+    std::vector<algorithm::TargetParameter>* targetParameters =
+            algorithmConfigurator.readAlgorithmSequenceFromFile(
+                    "../res/algorithm_config_full.json", algorithm::ConfigFileTypeJson);
+
+    implicitDifferenceMethod->setTargetParameters(targetParameters);
+    explicitDifferenceMethod->setTargetParameters(targetParameters);
+
+    std::cout << "*****Algorithms info*****" << std::endl;
+
+    std::chrono::time_point<std::chrono::system_clock> start;
+    std::chrono::time_point<std::chrono::system_clock> end;
+
+    start = std::chrono::system_clock::now();
+
+    std::cout << "Finite-difference method: " << std::endl << std::endl;
+    implicitDifferenceMethod->calcResult();
+
+    std::cout << "Tangential method: " << std::endl;
+    explicitDifferenceMethod->calcResult();
+
+    end = std::chrono::system_clock::now();
+
+    delete implicitDifferenceMethod;
+    delete explicitDifferenceMethod;
+
+    auto totalCalculationsTimeMillis = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    auto totalCalculationsTimeSecs = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+
+    std::cout << "***Total calculations time: " << totalCalculationsTimeMillis << " ms." << " ("
+              << totalCalculationsTimeSecs << " sec.)" << std::endl;
+}
+
+
+void makePlots(
+        std::vector<algorithm::Variables>& implicitExperimentVariables,
+        std::vector<algorithm::Variables>& explicitExperimentVariables,
+        std::vector<algorithm::IterationInfo>& implicitIterationsInfo,
+        std::vector<algorithm::IterationInfo>& explicitIterationsInfo,
+        bool needPauseExecution)
+{
+    plot::Plot* implicitMethodPlot = configMagneticFluidPlot(
+            implicitExperimentVariables, implicitIterationsInfo, "Finite-difference method",
+            plot::PlotOutputTypeWindow, "");
+    plot::Plot* explicitMethodPlot = configMagneticFluidPlot(
+            explicitExperimentVariables, explicitIterationsInfo, "Tangential method", plot::PlotOutputTypeWindow,
+            "");
+    plot::Plot* comparisonPlot = configComparisonPlot(
+            implicitExperimentVariables.back(), implicitIterationsInfo.back(), explicitExperimentVariables.back(),
+            explicitIterationsInfo.back(), "Comparison", plot::PlotOutputTypeWindow, "");
+
+    implicitMethodPlot->makeGraphs();
+    explicitMethodPlot->makeGraphs();
+    comparisonPlot->makeGraphs();
+
+    if (needPauseExecution)
+    {
+        utils::pauseExecution();
+    }
+
+    delete explicitMethodPlot;
+    delete implicitMethodPlot;
+    delete comparisonPlot;
+}
+
+
+void calcResiduals(
+        std::vector<algorithm::Variables>& implicitExperimentVariables,
+        std::vector<algorithm::Variables>& explicitExperimentVariables,
+        std::vector<algorithm::IterationInfo>& implicitIterationsInfo,
+        std::vector<algorithm::IterationInfo>& explicitIterationsInfo)
+{
+    std::cout << "*****Residuals*****" << std::endl;
+    auto size = std::min(explicitIterationsInfo.size(), implicitIterationsInfo.size());
+    for (auto i = 0; i < size; i++)
+    {
+        auto currentOldIteration = implicitIterationsInfo[i].index;
+        auto currentNewIteration = explicitIterationsInfo[i].index;
+
+        double radiusResidual = algorithm::calcResidual(implicitExperimentVariables[currentOldIteration].r,
+                                                        explicitExperimentVariables[currentNewIteration].r);
+        double heightResidual = algorithm::calcResidual(implicitExperimentVariables[currentOldIteration].z,
+                                                        explicitExperimentVariables[currentNewIteration].z);
+        double commonResidual = std::max(radiusResidual, heightResidual);
+
+        std::cout << "Experiment number:" << std::endl;
+        std::cout << "Finite-difference method: #" << currentOldIteration << std::endl;
+        std::cout << "Tangential method: #" << currentNewIteration << std::endl;
+        std::cout << "Residual:" << std::endl;
+        std::cout << "radius: " << radiusResidual << ", height: " << heightResidual << std::endl;
+        std::cout << "common: " << commonResidual << std::endl;
+        std::cout << "************************" << std::endl;
+    }
 }
